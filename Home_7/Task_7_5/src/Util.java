6d555a52a9308a89774fbb197ab98f4cf88f7563
Yauheni <yauhenij@gmail.com>
2021-11-05 11:55:17 +0300

    Исправлен класс Util на использование StringBuilder

--- Home_7/Task_7_5/src/Util.java
+++ Home_7/Task_7_5/src/Util.java
@@ -1,10 +1,28 @@
 public class Util { //создал отдельный класс, чтобы формировать строку для письма
     public static String createMessage(final Patient patient, final boolean covidTest, final int antibodiesCount) {
-        String aboutTest = covidTest ? ", unfortunately test is POSITIVE. Stay at home at 7 days. " : ", fortunately, test is NEGATIVE. Enjoy your regular life. ";
-        String aboutAntibodies = (antibodiesCount > 0) ? "Your antibodies count is: " + antibodiesCount : "You have no antibodies.";
-        return "Dear " + //я сначала хотел сделать через StringBuilder, но Idea посоветовала просто стринг вернуть - мне кажется, она права, так лучше выглядит
-                patient.getPatientName() +
-                aboutTest +
-                aboutAntibodies;
+//        String aboutTest = covidTest ? ", unfortunately test is POSITIVE. Stay at home at 7 days. " : ", fortunately, test is NEGATIVE. Enjoy your regular life. ";
+//        String aboutAntibodies = (antibodiesCount > 0) ? "Your antibodies count is: " + antibodiesCount : "You have no antibodies.";
+//        return "Dear " + //я сначала хотел сделать через StringBuilder, но Idea посоветовала просто стринг вернуть - мне кажется, она права, так лучше выглядит
+//                patient.getPatientName() +
+//                aboutTest +
+//                aboutAntibodies;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Dear ");
+        stringBuilder.append(patient.getPatientName());
+
+        if (covidTest) {
+            stringBuilder.append(", unfortunately test is POSITIVE. Stay at home at 7 days. ");
+        } else {
+            stringBuilder.append(", fortunately, test is NEGATIVE. Enjoy your regular life. ");
+        }
+
+        if (antibodiesCount > 0) {
+            stringBuilder.append("Your antibodies count is: ");
+            stringBuilder.append(antibodiesCount);
+        } else {
+            stringBuilder.append("You have no antibodies.");
+        }
+
+        return stringBuilder.toString();
     }
 }
