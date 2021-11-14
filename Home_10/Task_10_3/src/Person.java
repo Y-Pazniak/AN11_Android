9f62d5700f8f4fd9857b261e72c9d4e0bfcdf016
Yauheni <yauhenij@gmail.com>
2021-11-14 15:20:20 +0300

    3) 	а) Создать динамический массив (ArrayList), содержащий объекты класса Person. б) Распечатать его содержимое используя foreach. в) Изменить имя второго Person. г) Создать нового Person и проверить есть ли такой Person в коллекции д) Удалить последний Person. е) Удалить все ящики.

---
+++ Home_10/Task_10_3/src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private String name;
+
+    public Person(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
